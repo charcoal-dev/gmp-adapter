6fa31edd725a1fb9828152f352a0689cfced9914
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 00:46:45 +0500

    Update `BigIntHelper::inRange` to return GMP objects instead of decimal strings

--- src/Support/BigIntHelper.php
+++ src/Support/BigIntHelper.php
@@ -14,19 +14,19 @@ namespace Charcoal\Adapters\Gmp\Support;
 final readonly class BigIntHelper
 {
     /**
-     * Check if a value is within the range, accepts int|strings (any GMP supported base)
-     * Guarantees that values falling within the allowed range will be returned as decimal strings.
+     * Check if a value is within the range; accepts int|strings (any GMP supported base)
+     * Guarantees that values falling within the allowed range will be returned as a GMP object.
      */
-    public static function inRange(int|string $value, int|string $min, int|string $max): false|string
+    public static function inRange(int|string|\GMP $value, int|string|\GMP $min, int|string|\GMP $max): false|\GMP
     {
-        $value = gmp_init($value, 10);
-        $min = gmp_init($min, 10);
-        $max = gmp_init($max, 10);
+        $value = $value instanceof \GMP ? $value : gmp_init($value, 10);
+        $min = $min instanceof \GMP ? $min : gmp_init($min, 10);
+        $max = $max instanceof \GMP ? $max : gmp_init($max, 10);
         if (gmp_cmp($min, $max) > 0) {
             throw new \InvalidArgumentException("Minimum value must be less than maximum value");
         }
 
-        return gmp_cmp($value, $min) >= 0 && gmp_cmp($value, $max) <= 0 ?
-            gmp_strval($value, 10) : false;
+        return (gmp_cmp($value, $min) >= 0 && gmp_cmp($value, $max) <= 0) ?
+            $value : false;
     }
 }
