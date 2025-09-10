9d2adec54e22c7b683c527aefea69a33461f6e96
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 01:33:18 +0500

    Update `BigIntHelper::inRange` to return boolean instead of GMP object or false.

--- src/Support/BigIntHelper.php
+++ src/Support/BigIntHelper.php
@@ -15,9 +15,8 @@ final readonly class BigIntHelper
 {
     /**
      * Check if a value is within the range; accepts int|strings (any GMP supported base)
-     * Values falling within the allowed range will be returned as a GMP object.
      */
-    public static function inRange(int|string|\GMP $value, int|string|\GMP $min, int|string|\GMP $max): false|\GMP
+    public static function inRange(int|string|\GMP $value, int|string|\GMP $min, int|string|\GMP $max): bool
     {
         $value = $value instanceof \GMP ? $value : gmp_init($value);
         $min = $min instanceof \GMP ? $min : gmp_init($min);
@@ -26,7 +25,6 @@ final readonly class BigIntHelper
             throw new \InvalidArgumentException("Minimum value must be less than maximum value");
         }
 
-        return (gmp_cmp($value, $min) >= 0 && gmp_cmp($value, $max) <= 0) ?
-            $value : false;
+        return gmp_cmp($value, $min) >= 0 && gmp_cmp($value, $max) <= 0;
     }
 }
